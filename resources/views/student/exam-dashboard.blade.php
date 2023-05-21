9beb96d8d86d0121614bd59fe65cc02c65d07655
Varogell <alvaroivanka@gmail.com>
2023-05-21 21:08:11 +0700

    add countdown timer on exam

--- resources/views/student/exam-dashboard.blade.php
+++ resources/views/student/exam-dashboard.blade.php
@@ -3,8 +3,12 @@
 @extends('layouts.app')
 
 @section('content')
+    @php
+        $time = explode(':',$exam[0]['time']);
+    @endphp
     {{-- <p style="color:black">Welcome, {{ Auth::user()->name }}</p> --}}
     <p class="text-center">Tryout  :  {{ $exam[0]['exam_name'] }}</p>
+    <h4 class="text-righ time">{{ $exam[0]['time'] }}</h4>
     @php $qcount = 1; @endphp
     @if($success == true)
 
@@ -45,7 +49,34 @@
         $('.select_ans').click(function() {
             var no = $(this).attr('data-id');
             $('#ans_'+no).val($(this).val());
-        })
+        });
+        var time = @json($time);
+       $('.time').text(time[0]+':'+time[1]+':00 Left time');
+       
+       var seconds = 60;
+       var hours = time[0];
+       var minutes = time[1];
+       
+       setInterval(() => {
+
+            if(seconds <= 0){
+                minutes--;
+                seconds = 60;
+            }
+            if(minutes <= 0){
+                hours--;
+                minutes = 59;
+                seconds = 60;
+            }
+
+            let tempHours = hours.toString().length > 1 ? hours:'0'+hours;
+            let tempMinutes = minutes.toString().length > 1 ? minutes:'0'+minutes;
+            let tempSeconds = seconds.toString().length > 1 ? seconds:'0'+seconds;
+
+            $('.time').text(tempHours+':'+tempMinutes+':'+tempSeconds+'Left time');
+            seconds--;
+
+       }, 1000);
     });
 
     function isValid(){
