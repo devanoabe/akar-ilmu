0de0b475f2434e98016ed5793fe5efe62841cb4b
ValentinoMalakianno <93367868+ValentinoMalakianno@users.noreply.github.com>
2023-06-07 11:27:50 +0700

    Explaination Review

--- resources/views/student/result.blade.php
+++ resources/views/student/result.blade.php
@@ -67,6 +67,26 @@
         </div>
     </div>
 
+    <div class="modal fade" id="explainationModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Explaination</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                    <div class="modal-body">
+                        <p id="explaination"></p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+            </div>
+        </div>
+    </div>
+
     <script>
     $(document).ready(function(){
 
@@ -101,9 +121,15 @@
                                     <div class="row">
                                         <div class="col-sm-12">
                                             <h6>Q(`+(i+1)+`). `+data[i]['question']['soal']+`</h6>
-                                            <p>Ans: - `+answer+`  `+is_correct+`</p>
+                                            <p>Ans: - `+answer+`  `+is_correct+`</p>`;
+
+                                    if(data[i]['question']['explaination'] != null){
+                                        html +=`<p><a href="#" data-explaination="`+data[i]['question']['explaination']+`" class="explaination" data-toggle="modal" data-target="#explainationModal">Explaination</a></p>`;
+                                    }
+
+                                    html +=`
                                                 </div>
-                                                </div>
+                                            </div>
                                     `;
                                 }
 
@@ -118,6 +144,11 @@
                         $('.review-qna').html(html);
                     }
                 });
+            });
+
+            $(document).on('click','.explaination',function(){
+                var explaination = $(this).attr('data-explaination');
+                $('#explaination').text(explaination);
             });
         });
         </script>
